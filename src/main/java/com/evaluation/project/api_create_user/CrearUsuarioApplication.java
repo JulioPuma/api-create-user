06dd4175c8002bf7e7cd99da1451b565636edf24
Julio <jcpumahuacre@gmail.com>
2025-08-28 20:10:44 -0500

    add archetype

---
+++ src/main/java/com/evaluation/project/api_create_user/CrearUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.evaluation.project.api_create_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrearUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrearUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evaluation/project/api_create_user/CrearUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluation.project.api_create_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrearUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
